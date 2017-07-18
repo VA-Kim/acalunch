10f1af218b7ed1b4e46b8574beb67648d2d5df44
viewa <icewkd@gmail.com>
2017-07-18 15:54:39 +0900

    css&html

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+// index.js
+
 var express = require('express');
 var app = express();
 
-app.get('/', function(req,res){
-  res.send('Hello World!');
-});
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000, function(){ //3000번 포트를 사용합니다.
  console.log('Server On!'); //서버가 실행되면 콘솔창에 표시될 메세지입니다.
